dae7857554c3adb40a31289c8d7a8d583d39f6e8
unknown <yousra.mashkoor@gmail.com>
2020-10-01 15:32:22 +0500

    Chat app

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from "react";
+import { View, SafeAreaView } from "react-native";
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  MessageList,
+  MessageInput,
+} from "stream-chat-expo";
+
+const chatClient = new StreamChat('f8wwud5et5jd');
+const userToken =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidGhyb2JiaW5nLWxpbWl0LTMifQ.TVETnQ45Jsmskz7ZPhFeYgdvzWAL5z3gVps_7Q_iV4Q';
+
+const user = {
+  id: 'throbbing-limit-3',
+  name: 'Throbbing limit',
+  image:
+    'https://getstream.io/random_png/?id=throbbing-limit-3&amp;name=Throbbing+limit',
+};
+
+chatClient.setUser(user, userToken);
+
+class ChannelScreen extends React.Component {
+  render() {
+    const channel = chatClient.channel("messaging", "throbbing-limit-3");
+    channel.watch();
+
+    return (
+      <SafeAreaView>
+        <Chat client={chatClient}>
+          <Channel channel={channel}>
+            <View style={{ display: "flex", height: "100%" }}>
+              <MessageList />
+              <MessageInput />
+            </View>
+          </Channel>
+        </Chat>
+      </SafeAreaView>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends React.Component {
+  render() {
+    return <ChannelScreen />;
+  }
+}
